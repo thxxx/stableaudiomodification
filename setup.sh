853da60a340631705f4e2b229e1cff7775d80d54
thxxx <zxcv05999@naver.com>
2024-10-13 06:46:51 +0000

    setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+apt-get -y update
+git config --global user.email zxcv05999@naver.com
+git config --global user.name thxxx
+apt-get install -y libsndfile1-dev
+python -m pip install --upgrade pip
+pip install diffusers transformers einops
+pip install .
